963ec5ed865e64a83ca974d5455ac371c572bdaa
Sarah Givens <sgivens@avvo.com>
2019-10-16 12:57:27 -0700

    finish articles pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
-  def show
-    @article = Article.find(params[:id])
-  end
-
   def index
     @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   def new
@@ -24,6 +24,7 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
   def update
     @article = Article.find(params[:id])
 
@@ -33,7 +34,12 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
-  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
   private
     def article_params
       params.require(:article).permit(:title, :text)
